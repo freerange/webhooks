a0b974196ef4e9f0d23daa71622b8a85dc7b6b3c
James Mead <james@floehopper.org>
2014-06-17 11:15:54 +0100

    Extract task local variable.

--- app.rb
+++ app.rb
@@ -25,14 +25,11 @@ post '/harmonia/assignments' do
   attributes = JSON.parse(json)
 
   assignment = attributes['assignment']
-  task_name = assignment['task']['name']
-  task_instructions = assignment['task']['instructions']
-  task_due_at = assignment['task']['due_at']
-  person_name = assignment['person']['name']
-  member_id = harmonia_person_names_vs_trello_member_ids[person_name]
+  task, person = assignment['task'], assignment['person']
+  member_id = harmonia_person_names_vs_trello_member_ids[person['name']]
 
-  card = Trello::Card.create(:name => task_name, :list_id => ENV['TRELLO_LIST_ID'], :desc => task_instructions, :member_ids => [member_id])
-  card.due = task_due_at
+  card = Trello::Card.create(:name => task['name'], :list_id => ENV['TRELLO_LIST_ID'], :desc => task['instructions'], :member_ids => [member_id])
+  card.due = task['due_at']
   card.update!
 
   [200, 'OK']
