7b962c366de32e9b00bd69ec6be3180cc3788ecd
James Mead <james@floehopper.org>
2014-07-15 12:40:12 +0100

    Use guard condition instead of if/else for auth token.

--- app.rb
+++ app.rb
@@ -22,23 +22,23 @@ get '/' do
 end
 
 post '/harmonia/assignments' do
-  if params[:token] == authentication_token
-    json = request.body.read
-    attributes = JSON.parse(json)
-
-    assignment = attributes['assignment']
-    task, person = assignment['task'], assignment['person']
-    member_id = harmonia_person_names_vs_trello_member_ids[person['name']]
-
-    unless task['done']
-      card = Trello::Card.create(:name => task['name'], :list_id => ENV['TRELLO_LIST_ID'], :desc => task['instructions'])
-      card.due = task['due_at']
-      card.add_member(Trello::Member.new('id' => member_id))
-      card.update!
-    end
-
-    [200, 'OK']
-  else
-    [401, 'Unauthorized']
+  unless params[:token] == authentication_token
+    return [401, 'Unauthorized']
   end
+
+  json = request.body.read
+  attributes = JSON.parse(json)
+
+  assignment = attributes['assignment']
+  task, person = assignment['task'], assignment['person']
+  member_id = harmonia_person_names_vs_trello_member_ids[person['name']]
+
+  unless task['done']
+    card = Trello::Card.create(:name => task['name'], :list_id => ENV['TRELLO_LIST_ID'], :desc => task['instructions'])
+    card.due = task['due_at']
+    card.add_member(Trello::Member.new('id' => member_id))
+    card.update!
+  end
+
+  [200, 'OK']
 end
