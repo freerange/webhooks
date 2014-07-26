0cb3c42072b9322dd0a6b4fdef6c6053d178548d
James Mead <james@floehopper.org>
2014-07-26 15:26:20 +0100

    Prevent real HTTP requests when running specs.

--- Gemfile
+++ Gemfile
@@ -13,4 +13,5 @@ end
 group :test do
   gem 'rspec'
   gem 'rack-test'
+  gem 'webmock'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@ ENV['RACK_ENV'] = 'test'
 require_relative '../app.rb'
 require 'rspec'
 require 'rack/test'
+require 'webmock'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
 end
+
+WebMock.disable_net_connect!
