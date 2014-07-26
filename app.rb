0af3e38d392127faad3c8c710c756094c0a87c8d
James Mead <james@floehopper.org>
2014-07-26 15:27:17 +0100

    Add some specs around the Harmonia assignments action.
    Note that I made some changes to the application code in this commit in order
    to make it easier to test:
    
    * I converted a bunch of settings which were being stored in local variables
    into proper Sinatra settings so they could be accessed from the specs.
    * I changed the "dictionary" of Harmonia people names vs Trello member IDs to
    instantiate `Trello::Member` objects.
    * I moved the `card.update!` statement inside the conditional where the due
    date is set, because I realised that `Trello::Card#remove_member` and
    `Trello::Card#add_member` do not rely on this call.

--- app.rb
+++ app.rb
@@ -12,19 +12,20 @@ logger = Logger.new(File.expand_path('../log/webhooks.log', __FILE__))
 Dotenv.load
 
 harmonia_person_names_vs_trello_member_ids = JSON.parse(ENV.fetch('HARMONIA_PERSON_NAMES_VS_TRELLO_MEMBER_IDS'))
-trello_list_id = ENV.fetch('TRELLO_LIST_ID')
-authentication_token = ENV.fetch('AUTHENTICATION_TOKEN')
-trello_key = ENV.fetch('TRELLO_KEY')
-trello_token = ENV.fetch('TRELLO_TOKEN')
-trello_secret = ENV.fetch('TRELLO_SECRET')
-host = ENV.fetch('HOST')
+set :harmonia_person_names_vs_trello_members, Hash[harmonia_person_names_vs_trello_member_ids.map { |name, id| [name, Trello::Member.new('id' => id)] }]
+set :trello_list_id, ENV.fetch('TRELLO_LIST_ID')
+set :authentication_token, ENV.fetch('AUTHENTICATION_TOKEN')
+set :trello_key, ENV.fetch('TRELLO_KEY')
+set :trello_token, ENV.fetch('TRELLO_TOKEN')
+set :trello_secret, ENV.fetch('TRELLO_SECRET')
+set :host, ENV.fetch('HOST')
 
-trello_events_path = '/trello/events'
-trello_events_url = "http://#{host}#{trello_events_path}"
+set :trello_events_path, '/trello/events'
+set :trello_events_url, "http://#{settings.host}#{settings.trello_events_path}"
 
 Trello.configure do |config|
-  config.developer_public_key = trello_key
-  config.member_token = trello_token
+  config.developer_public_key = settings.trello_key
+  config.member_token = settings.trello_token
 end
 
 get '/' do
@@ -32,7 +33,7 @@ get '/' do
 end
 
 post '/harmonia/assignments' do
-  unless params[:token] == authentication_token
+  unless params[:token] == settings.authentication_token
     return [401, 'Unauthorized']
   end
 
@@ -41,40 +42,40 @@ post '/harmonia/assignments' do
 
   assignment = attributes['assignment']
   task, person = assignment['task'], assignment['person']
-  member_id = harmonia_person_names_vs_trello_member_ids[person['name']]
+  member = settings.harmonia_person_names_vs_trello_members[person['name']]
 
   unless task['done']
     task_url = "https://harmonia.io/t/#{task['key']}"
     task_link = "Harmonia task: #{task_url}"
-    list = Trello::List.find(trello_list_id)
+    list = Trello::List.find(settings.trello_list_id)
     card = list.cards.detect { |c| c.desc =~ Regexp.new(task_link) }
     if card
       card.members.each { |m| card.remove_member(m) }
     else
       description = [task['instructions'], '', task_link].join("\n")
-      card = Trello::Card.create(:name => task['name'], :list_id => trello_list_id, :desc => description)
+      card = Trello::Card.create(:name => task['name'], :list_id => list.id, :desc => description)
       webhook = Trello::Webhook.create(
         :description => "Watch card #{card.id}",
         :id_model => card.id,
-        :callback_url => trello_events_url
+        :callback_url => settings.trello_events_url
       )
       card.due = task['due_at']
+      card.update!
     end
-    card.add_member(Trello::Member.new('id' => member_id))
-    card.update!
+    card.add_member(member)
   end
 
   [200, 'OK']
 end
 
-head trello_events_path do
+head settings.trello_events_path do
   [200, 'OK']
 end
 
-post trello_events_path do
+post settings.trello_events_path do
   body = request.body.read
-  data = body + trello_events_url
-  hash = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'), trello_secret, data)
+  data = body + settings.trello_events_url
+  hash = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'), settings.trello_secret, data)
   signature = Base64.encode64(hash).chomp
   unless request.env['HTTP_X_TRELLO_WEBHOOK'] == signature
     return [401, 'Unauthorized']
---
+++ spec/integration/harmonia_assignments_spec.rb
@@ -0,0 +1,114 @@
+require_relative '../spec_helper'
+
+describe 'Harmonia Assignments' do
+  let(:app) { Sinatra::Application }
+  let(:authentication_token) { app.settings.authentication_token }
+  let(:person) { { name: 'James Mead' } }
+  let(:task) { { name: 'task-name', key: 'abc123', instructions: 'task-instructions', due_at: "2014-07-05T17:00:00.000+01:00", done: false } }
+  let(:task_url) { "https://harmonia.io/t/#{task[:key]}" }
+  let(:task_link) { "Harmonia task: #{task_url}" }
+  let(:body) { { assignment: { task: task, person: person } }.to_json }
+  let(:cards) { [] }
+  let(:list) { double(:trello_list, id: app.settings.trello_list_id, cards: cards) }
+  let(:card) { double(:trello_card, id: 'def456').as_null_object }
+
+  before do
+    allow(Trello::List).to receive(:find).and_return(list)
+    allow(Trello::Card).to receive(:create).and_return(card)
+    allow(Trello::Webhook).to receive(:create)
+  end
+
+  it 'creates a new Trello card corresponding to the Harmonia task' do
+    expect(Trello::Card).to receive(:create).with(name: 'task-name', list_id: list.id, desc: include(task[:instructions]).and(include(task_link)))
+
+    post path, body
+  end
+
+  it 'creates a new Trello webhook to monitor the newly created Trello card' do
+    expect(Trello::Webhook).to receive(:create).with(include(id_model: card.id, callback_url: app.settings.trello_events_url))
+
+    post path, body
+  end
+
+  it 'updates the newly created Trello card corresponding to the Harmonia task' do
+    expect(card).to receive(:due=).with(task[:due_at])
+    expect(card).to receive(:update!)
+
+    post path, body
+  end
+
+  it 'adds the assignee as a member of the newly created Trello card' do
+    member = app.settings.harmonia_person_names_vs_trello_members[person[:name]]
+    expect(card).to receive(:add_member).with(member)
+
+    post path, body
+  end
+
+  it 'responds with 200 OK' do
+    post path, body
+
+    expect(last_response.status).to eq(200)
+  end
+
+  context 'Trello card already exists' do
+    let(:member_one) { double(:member) }
+    let(:member_two) { double(:member) }
+    let(:existing_card) { double(:existing_card, desc: task_link, members: [member_one, member_two]) }
+    let(:cards) { [existing_card] }
+
+    before do
+      allow(existing_card).to receive(:remove_member)
+      allow(existing_card).to receive(:add_member)
+    end
+
+    it 'removes all members of existing card' do
+      expect(existing_card).to receive(:remove_member).with(member_one)
+      expect(existing_card).to receive(:remove_member).with(member_two)
+
+      post path, body
+    end
+
+    it 'adds the assignee as a member of the existing Trello card' do
+      member = app.settings.harmonia_person_names_vs_trello_members[person[:name]]
+      expect(existing_card).to receive(:add_member).with(member)
+
+      post path, body
+    end
+  end
+
+  context 'Harmonia assignment is already done' do
+    let(:task) { { done: true } }
+
+    it 'responds with 200 OK' do
+      post path, body
+
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'does not create a Trello card' do
+      post path, body
+
+      expect(Trello::Card).not_to receive(:create)
+    end
+
+    it 'does not create a Trello webhook' do
+      post path, body
+
+      expect(Trello::Webhook).not_to receive(:create)
+    end
+  end
+
+  context 'incorrect authentication token' do
+    let(:authentication_token) { app.settings.authentication_token + '-incorrect' }
+
+    it 'responds with 401 Unauthorized' do
+      post path, body
+
+      expect(last_response.status).to eq(401)
+    end
+  end
+
+  def path
+    "/harmonia/assignments?token=#{authentication_token}"
+  end
+end
